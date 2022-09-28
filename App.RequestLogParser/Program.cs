52d7b81405e644372ec48dfc1b5c7b636d94d4c3
James Chen <james@innovue.ltd>
2022-09-27 23:35:07 -0700

    (^) added basic program to test parser

--- App.RequestLogParser/Program.cs
+++ App.RequestLogParser/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Collections.Generic;
+using System.Text.Json;
+using RequestLogParser.Lib.Utilities;
+
+var inputPath = "./App.RequestLogParser/test.txt";
+var parser = new LogFileParser(Path.GetFullPath(inputPath), quotedFields: true);
+var logItems = parser.ReadLogItems();
+
+
+Console.WriteLine(JsonSerializer.Serialize(logItems));
