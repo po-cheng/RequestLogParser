f6499477290f1ce13a6f894099934faa56d8e214
James Chen <james@innovue.ltd>
2022-09-28 23:52:15 -0700

    (^) added new test case for request logs that does not providde the protocol used #3

--- RequestLogParser.Lib.Test/LogParserTest.cs
+++ RequestLogParser.Lib.Test/LogParserTest.cs
@@ -9,6 +9,7 @@ public class LogParserTest
   [Theory]
   [InlineData("Data/log-item-sample.txt", "141.243.1.172", "2022-08-29T23:53:25-04:00", "GET", "/Software.html", 200, 1497)]
   [InlineData("Data/404-item-sample.txt", "eso2.orl.mmc.com", "2022-08-30T15:00:35-04:00", "GET", "/docs/convert.html", 404, 0)]
+  [InlineData("Data/no-protocol-sample.txt", "141.243.1.172", "2022-08-29T23:53:25-04:00", "GET", "/Software.html", 200, 1497)]
   public void ResultModelShouldMatch(string filePath, string host, string timrStr, string method, string path, int status, int size)
   {
     var parser = new LogFileParser(filePath, quotedFields: true);
