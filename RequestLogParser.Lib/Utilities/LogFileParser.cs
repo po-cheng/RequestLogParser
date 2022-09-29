2f62349ba84f5ba30abc23c569087cf18f65034b
James Chen <james@innovue.ltd>
2022-09-28 23:46:08 -0700

    (^) changed the regex thats used to separate method and path to handle when the protorol isnt specified #3

--- RequestLogParser.Lib/Utilities/LogFileParser.cs
+++ RequestLogParser.Lib/Utilities/LogFileParser.cs
@@ -36,8 +36,8 @@ public class LogFileParser
         var ts = @$"2022-08-{fields[1]} -04:00";
         var time = DateTime.ParseExact(ts, "yyyy-MM-[dd:HH:mm:ss] zzz", CultureInfo.CurrentCulture).ToUniversalTime();
 
-        // separate method and path
-        var pathRegex = new Regex(@"(?<method>[A-Z]+)\s(?<path>.+)\s");
+        // separate method, path, and protocol
+        var pathRegex = new Regex(@"(?<method>[A-Z]+)\s(?<path>\S+)");
         var match = pathRegex.Match(fields[2]);
         var method = match.Groups["method"]?.Value ?? "";
         var reqPath = match.Groups["path"]?.Value ?? "";
